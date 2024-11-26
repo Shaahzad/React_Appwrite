7deb11481d602f3ec21e724274d70b71f13828db
Muhammad shahzad <120109925+Shaahzad@users.noreply.github.com>
2024-11-26 14:13:21 +0500

    Update Signup.jsx

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -28,8 +28,8 @@ const navigate = useNavigate()
         <div className='flex justify-center items-center h-screen'>
           <div className='flex flex-col gap-4 shadow-2xl px-10 py-10'>
           <h1 className='text-2xl font-bold'>Signup</h1>   
-          <input type="text" placeholder='Enter Email' className='border border-black outline-none px-2 py-2' onChange={(e) => setEmail(e.target.value)} value={email}/>
-          <input type="password" placeholder='Enter Password' className='border border-black outline-none px-2 py-2' onChange={(e) => setPassword(e.target.value)} value={password}/>
+          <input type="text" placeholder='Enter Email @mail.com' className='border border-black outline-none px-2 py-2' onChange={(e) => setEmail(e.target.value)} value={email}/>
+          <input type="password" placeholder='Enter Password ****' className='border border-black outline-none px-2 py-2' onChange={(e) => setPassword(e.target.value)} value={password}/>
           <button className='bg-black text-white px-2 py-2 rounded-lg font-bold' onClick={Handlesignup}>Submit</button>
           </div>
         </div>
