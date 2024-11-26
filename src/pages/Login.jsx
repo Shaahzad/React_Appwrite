07b4bc021095edfaafdadc36c987b350cbec9e8f
Muhammad shahzad <120109925+Shaahzad@users.noreply.github.com>
2024-11-26 14:10:06 +0500

    Update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -19,8 +19,8 @@ const Login = () => {
     <div className='flex justify-center items-center h-screen'>
     <div className='flex flex-col gap-4 shadow-2xl px-10 py-10'>
     <h1 className='text-2xl font-bold'>Login</h1>
-    <input type="text" placeholder='Enter Email' className='border border-black outline-none px-2 py-2' onChange={(e) => setEmail(e.target.value)}/>
-    <input type="password" placeholder='Enter Password' className='border border-black outline-none px-2 py-2' onChange={(e) => setPassword(e.target.value)}/>
+    <input type="text" placeholder='Enter Email @ mail.com' className='border border-black outline-none px-2 py-2' onChange={(e) => setEmail(e.target.value)}/>
+    <input type="password" placeholder='Enter Password ****' className='border border-black outline-none px-2 py-2' onChange={(e) => setPassword(e.target.value)}/>
     <button className='bg-black text-white px-2 py-2 rounded-lg font-bold' onClick={loginHandler}>Submit</button>
     </div>
   </div>
